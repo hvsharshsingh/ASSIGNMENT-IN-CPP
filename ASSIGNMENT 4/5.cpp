03179f811cd71b7aa55832565c62475a6e11cd87
Harsh Vardhan Singh <145780320+hvsharshsingh@users.noreply.github.com>
2023-12-23 12:18:24 +0530

    Update 5.cpp

--- ASSIGNMENT 4/5.cpp
+++ ASSIGNMENT 4/5.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 class StringComparator {
 private:
     string str1;
     string str2;
-
 public:
-    // Constructor to initialize the strings
     StringComparator(const string& s1, const string& s2) : str1(s1), str2(s2) {}
 
     // Member function to compare the two strings
@@ -19,10 +16,8 @@ public:
 };
 
 int main() {
-    // Create an object of the StringComparator class
     StringComparator stringComparatorObject("Hello", "World");
 
-    // Compare the two strings
     int result = stringComparatorObject.compareStrings();
 
     // Print the result of the comparison
